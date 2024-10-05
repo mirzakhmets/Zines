3db2dd8f1dd7c507eac345a97bea2381850789c5
mirzakhmets <mirzakhmets@icloud.com>
2024-10-05 15:54:12 +0500

    Added trial

--- com/zines/Main.java
+++ com/zines/Main.java
@@ -1,9 +1,55 @@
 /*     */ package com.zines;
-/*     */ import java.io.BufferedReader;
-/*     */ import java.io.File;
-/*     */ import java.io.FileReader;
-/*     */ 
+/*     */ import java.io.*;
+/*     */
+/*     */
+
+/*     */
 /*     */ public class Main {
+    public static boolean isRegistered() {
+        try {
+            File file = new File(System.getProperty("user.home") + "/register.lic");
+
+            if (file.exists()) {
+                return true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return false;
+    }
+
+    public static void checkRuns() {
+        try {
+            File file = new File(System.getProperty("user.home") + "/run.lic");
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+
+            FileInputStream fis = new FileInputStream(new File(System.getProperty("user.home") + "/run.lic"));
+
+            byte[] b = fis.readAllBytes();
+
+            if (b.length > 30) {
+                System.out.println("Number of runs expired. Please purchase the program (visit site https://ovg-developers.mystrikingly.com/).");
+                System.exit(0);
+            }
+
+            fis.close();
+
+            FileOutputStream fos = new FileOutputStream(new File(System.getProperty("user.home") + "/run.lic"));
+
+            fos.write(b);
+
+            fos.write(' ');
+
+            fos.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 /*     */   public static String readFile(String path) {
 /*   8 */     StringBuilder content = new StringBuilder();
 /*     */     
@@ -31,6 +77,10 @@
 /*     */ 
 /*     */   
 /*     */   public static void main(String[] args) {
+    if (!isRegistered()) {
+        checkRuns();
+    }
+
 /*  34 */     Engine engine = new Engine();
 /*  35 */     boolean doExact = false;
 /*  36 */     boolean doRecrusive = false;
